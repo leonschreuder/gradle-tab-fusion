66411b6afee3dd9693e9411d3ce69b6b48022959
meonlol <mail@leonmoll.de>
2017-08-25 12:38:03 +0200

    add writing tasks to file

--- buildSrc/src/main/java/de/leonmoll/gradle/TaskCachePlugin.java
+++ buildSrc/src/main/java/de/leonmoll/gradle/TaskCachePlugin.java
@@ -19,11 +19,11 @@ import java.util.concurrent.Callable;
 
 public class TaskCachePlugin implements Plugin<Project> {
 
-    //public static final String TASK_REPORT = "taskReport";
-    //public static final String PROPERTY_REPORT = "propertyReport";
-    //public static final String DEPENDENCY_REPORT = "dependencyReport";
-    //public static final String HTML_DEPENDENCY_REPORT = "htmlDependencyReport";
-    //public static final String PROJECT_REPORT = "projectReport";
+    public static final String TASK_REPORT = "taskReport";
+    public static final String PROPERTY_REPORT = "propertyReport";
+    public static final String DEPENDENCY_REPORT = "dependencyReport";
+    public static final String HTML_DEPENDENCY_REPORT = "htmlDependencyReport";
+    public static final String PROJECT_REPORT = "projectReport";
 
     public void apply(Project project) {
         project.getTasks().create("writeTasks", TaskCacheTask.class, (task) -> {
@@ -32,18 +32,18 @@ public class TaskCachePlugin implements Plugin<Project> {
             final ProjectReportsPluginConvention convention = new ProjectReportsPluginConvention(project);
             project.getConvention().getPlugins().put("projectReports", convention);
 
-            //TaskReportTask taskReportTask = project.getTasks().create(TASK_REPORT, TaskReportTask.class);
-            //taskReportTask.setDescription("Generates a report about your tasks.");
-            //taskReportTask.conventionMapping("outputFile", new Callable<Object>() {
-            //    public Object call() throws Exception {
-            //        return new File(convention.getProjectReportDir(), "tasks.txt");
-            //    }
-            //});
-            //taskReportTask.conventionMapping("projects", new Callable<Object>() {
-            //    public Object call() throws Exception {
-            //        return convention.getProjects();
-            //    }
-            //});
+//            TaskReportTask taskReportTask = project.getTasks().create(TASK_REPORT, TaskReportTask.class);
+//            taskReportTask.setDescription("Generates a report about your tasks.");
+//            taskReportTask.conventionMapping("outputFile", new Callable<Object>() {
+//                public Object call() throws Exception {
+//                    return new File(convention.getProjectReportDir(), "tasks.txt");
+//                }
+//            });
+//            taskReportTask.conventionMapping("projects", new Callable<Object>() {
+//                public Object call() throws Exception {
+//                    return convention.getProjects();
+//                }
+//            });
 
             //PropertyReportTask propertyReportTask = project.getTasks().create(PROPERTY_REPORT, PropertyReportTask.class);
             //propertyReportTask.setDescription("Generates a report about your properties.");
--- buildSrc/src/main/java/de/leonmoll/gradle/TaskCacheTask.java
+++ buildSrc/src/main/java/de/leonmoll/gradle/TaskCacheTask.java
@@ -1,10 +1,7 @@
 package de.leonmoll.gradle;
 
 import org.gradle.api.Project;
-import org.gradle.api.Rule;
 import org.gradle.api.internal.project.ProjectTaskLister;
-import org.gradle.api.internal.tasks.options.Option;
-import org.gradle.api.tasks.Console;
 import org.gradle.api.tasks.diagnostics.AbstractReportTask;
 import org.gradle.api.tasks.diagnostics.internal.AggregateMultiProjectTaskReportModel;
 import org.gradle.api.tasks.diagnostics.internal.DefaultGroupTaskReportModel;
@@ -15,12 +12,16 @@ import org.gradle.api.tasks.diagnostics.internal.TaskDetailsFactory;
 import org.gradle.api.tasks.diagnostics.internal.TaskReportRenderer;
 
 import javax.inject.Inject;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TaskCacheTask extends AbstractReportTask {
-//    private TaskReportRenderer renderer = new TaskReportRenderer();
+    private TaskReportRenderer renderer = new TaskReportRenderer();
 
-//    private boolean detail;
     private AggregateMultiProjectTaskReportModel aggregateModel;
     private TaskDetailsFactory taskDetailsFactory;
 
@@ -32,43 +33,28 @@ public class TaskCacheTask extends AbstractReportTask {
         return new TaskReportRenderer(); // Sure, this is violates LSP, but i'm not really rendering any reports anyway
     }
 
-//    public void setRenderer(TaskReportRenderer renderer) {
-//        this.renderer = renderer;
-//    }
-
-//    @Option(option = "all", description = "Show additional tasks and detail.")
-//    public void setShowDetail(boolean detail) {
-//        this.detail = detail;
-//    }
-
-//    @Console
-//    public boolean isDetail() {
-//        return detail;
-//    }
-
     @Override
     public void generate(Project project) throws IOException {
-        //setShowDetail(true);
-        //renderer.showDetail(isDetail());
-        //renderer.addDefaultTasks(project.getDefaultTasks()); //defaultTasks is empty
+        List<String> taskList = new ArrayList<String>();
 
-        DefaultGroupTaskReportModel model = buildModelForAllTasks(project);
+        DefaultGroupTaskReportModel model = buildReportModelForAllTasks(project);
 
         for (String group : model.getGroups()) {
-            //renderer.startTaskGroup(group); //only prints headers
             for (TaskDetails task : model.getTasksForGroup(group)) {
-                project.getLogger().lifecycle(task.getPath().toString());
-                //renderer.addTask(task);
+                taskList.add(task.getPath().toString());
             }
         }
-        //renderer.completeTasks();
 
-        //for (Rule rule : project.getTasks().getRules()) {
-        //    renderer.addRule(rule);
-        //}
+        File outFile = new File(project.getBuildDir(), "tasks.txt");
+        writeStringToFile(outFile, String.join(" ", taskList));
+
+        //TODO: Completion for rules probably needs more complex logic, implement if actually needed
+//        for (Rule rule : project.getTasks().getRules()) {
+//            project.getLogger().lifecycle(rule.getDescription());
+//        }
     }
 
-    private DefaultGroupTaskReportModel buildModelForAllTasks(Project project) {
+    private DefaultGroupTaskReportModel buildReportModelForAllTasks(Project project) {
         //Create task model in which those for all projects are combinde
         aggregateModel = new AggregateMultiProjectTaskReportModel(false, true);
         taskDetailsFactory = new TaskDetailsFactory(project);
@@ -90,6 +76,26 @@ public class TaskCacheTask extends AbstractReportTask {
         SingleProjectTaskReportModel projectTaskModel = new SingleProjectTaskReportModel(taskDetailsFactory);
         projectTaskModel.build(getProjectTaskLister().listProjectTasks(project));
         aggregateModel.add(projectTaskModel);
+    }
+
+    private void writeStringToFile(File outFile, String fileContentString) {
+        BufferedWriter bw = null;
+        FileWriter fw = null;
+
+        try {
+            fw = new FileWriter(outFile);
+            bw = new BufferedWriter(fw);
+            bw.write(fileContentString);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (bw != null) bw.close();
+                if (fw != null) fw.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 
     @Inject
